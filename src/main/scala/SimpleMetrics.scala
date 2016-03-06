c8062a2c52681fc9ef78b326fb5ce2320a9679b4
Lun Liu <lunliu93@gmail.com>
2016-03-06 10:54:44 -0800

    Metrics merge

--- src/main/scala/SimpleMetrics.scala
+++ src/main/scala/SimpleMetrics.scala
@@ -7,23 +7,24 @@ object SimpleMetrics {
 		val conf = new SparkConf()
 		conf.setMaster("local[1]")
 		conf.setAppName("Simple Application")
-		
 		val sc = new SparkContext(conf)
 		val lines = sc.textFile("formatted")
 		// Below are for the avg identifier length
 		val lengths = lines.map(line => (line.split(':')(0), line.split(':')(1).length)).mapValues(x => (x,1))
 		val totalcount = lengths.reduceByKey((x,y) => (x._1 + y._1 , x._2 + y._2))
 		val avg = totalcount.map(a => (a._1, a._2._1 * 1.0 / a._2._2))
-		avg.saveAsTextFile("avg-length")
+		//avg.saveAsTextFile("avg-length")
 		// Below are for unique count
 		val unique = lines.distinct()
 		val uniqueCount = unique.map(line => (line.split(':')(0), 1)).reduceByKey(_ + _)
-		uniqueCount.saveAsTextFile("unique-count")
+		//uniqueCount.saveAsTextFile("unique-count")
 		// GitPython inputs
-		val gitpy = sc.textFile("gitpy")
-		val fixes = gitpy.map(line => line.split(':'))
-		val avg-rel = avg.join(fixes)
-		val uni-rel = unique.join(fixes)
+		val gitpy = sc.textFile("bugs.txt")
+		val fixes = gitpy.map(line => (line.split(':')(0), line.split(':')(1)))
+		val avg_rel = avg.join(fixes)
+		val uni_rel = uniqueCount.join(fixes)
+		avg_rel.saveAsTextFile("avg_rel")
+		uni_rel.saveAsTextFile("uni_rel")
 
 	}
 }
