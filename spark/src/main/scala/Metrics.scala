6059517107ad133295b65cf8f66025c1dd54e106
Lun Liu <lunliu93@gmail.com>
2016-03-12 14:05:39 -0800

    oops, forgot to update

--- spark/src/main/scala/Metrics.scala
+++ spark/src/main/scala/Metrics.scala
@@ -11,10 +11,11 @@ object Metrics {
 		conf.setMaster("local[1]")
 		conf.setAppName("Metrics Application")
 		val sc = new SparkContext(conf)
-		val lines = sc.textFile("updated")
+		val lines = sc.textFile("method_ids")
 
 		val words = lines.map(line => (line.split(':')(0) + ":" + line.split(':')(1), line.split(':')(2)))
-		val tokens = words.flatMapValues(word => word,split("_")).words.flatMapValues(word => word.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])"))
+		val tokens = words.flatMapValues(word => word.split("_")).flatMapValues(word => word.split("(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|(?<=[0-9])(?=[A-Z][a-z])|(?<=[a-zA-Z])(?=[0-9])"))
+		tokens.saveAsTextFile("tokens")
 
 		// Below are for the avg identifier length / per method 
 		//val lengths = lines.map(line => (line.split(':')(0) + ":" + line.split(':')(1), line.split(':')(2).length)).mapValues(x => (x,1))
